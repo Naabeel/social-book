4ce389ac17743b38f2511bfa51b894e9a9ecc183
Nabeel Ahmed <naabeelahhmed@gmail.com>
2023-06-26 19:16:29 +0530

    local auth

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,9 +2,24 @@ const User = require('../models/user');
 
 
 module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title:"user_profile"
-    })
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err,user){
+            if(err){console.log('error in finding use in cookies'); return};
+            if(user){
+                return res.render('user_profile',{
+                title:"user_profile",
+                user:user
+            })
+
+            }else{
+                return res.redirect('/users/sign-in')
+            }
+        })
+    }else{
+        return res.redirect('/users/sign-in');
+    }
+
+    
 }
 
 module.exports.signUp = function(req,res){
@@ -45,5 +60,20 @@ module.exports.create = function(req,res){
 }
 
 module.exports.createSession = function(req,res){
+    User.findOne({email:req.body.email},function(err, user){
+        if(err){console.log('error in finding user to sign in'); return}
+
+        if(user){
+            if(user.password != req.body.password){
+              return  res.redirect('back');
+            }
+
+            res.cookie('user_id', user.id);
+            return  res.redirect('/users/profile');
+        }
+        else{
+            return  res.redirect('back');
+        }
+    })
 
 }
